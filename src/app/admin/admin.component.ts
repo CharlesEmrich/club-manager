1f1b39640b6ef356c836f932fe34f726575ea324
Charles Emrich <libertinistic@gmail.com>
2017-07-14 14:06:06 -0700

    add admin route functionality

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MemberService } from '../member.service';
+import { Member } from '../member.model';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private memberService: MemberService) { }
 
   ngOnInit() {
   }
 
+  submitForm(title: string, artist: string, description: string) {
+    var newMember: Member = new Member(title, artist, description);
+    this.memberService.addMember(newMember);
+  }
 }
